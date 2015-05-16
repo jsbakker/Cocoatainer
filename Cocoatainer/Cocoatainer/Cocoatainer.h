8ad02ecc2eee8e9ba1c1da472b35f8ef080c8f4c
Jeffrey Bakker <jeffrey@seatoskyware.com>
2015-05-15 18:13:49 -0700

    Add workspace and rearrange cocoatainer exampler and cocoatainer as separate projects.

---
+++ Cocoatainer/Cocoatainer/Cocoatainer.h
@@ -0,0 +1,13 @@
+//
+//  Cocoatainer.h
+//  Cocoatainer
+//
+//  Created by Jeffrey Bakker on 2015-05-15.
+//  Copyright (c) 2015 Jeffrey Bakker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cocoatainer : NSObject
+
+@end
---
+++ Cocoatainer/Cocoatainer/Cocoatainer.m
@@ -0,0 +1,13 @@
+//
+//  Cocoatainer.m
+//  Cocoatainer
+//
+//  Created by Jeffrey Bakker on 2015-05-15.
+//  Copyright (c) 2015 Jeffrey Bakker. All rights reserved.
+//
+
+#import "Cocoatainer.h"
+
+@implementation Cocoatainer
+
+@end
